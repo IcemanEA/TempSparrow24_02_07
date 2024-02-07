01abbf905a2165ba9db9a181123123b2910333e1
Egor Ledkov <icemanea@gmail.com>
2024-02-07 15:12:05 +0300

    Start competition

---
+++ TempSparrow24_02_07/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TempSparrow24_02_07
+//
+//  Created by Egor Ledkov on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .white
+	}
+
+
+}
+
